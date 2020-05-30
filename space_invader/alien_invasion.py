cbb4485dcaa430d345a3f074fc6d03fe7ff7427e
Bo Sang <bsang@ualberta.ca>
2020-05-29 23:38:38 -0600

    Added ship class and image

--- space_invader/alien_invasion.py
+++ space_invader/alien_invasion.py
@@ -9,13 +9,12 @@ class AlienInvasion:
     def __init__(self):
         """Initialize game and create game resources."""
         pygame.init()
+        self.settings = Settings()
 
-
-        self.screen = pygame.display.set_mode((1280, 490))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width,self.settings.screen_height)
+        )
         pygame.display.set_caption("Alien Invasion")
-
-        # Set the background color
-        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -26,7 +25,7 @@ class AlienInvasion:
                     sys.exit()
 
                 # Redraw the screen during each pass through the loop.
-                self.screen.fill(self.bg_color)
+                self.screen.fill(self.settings.bg_color)
 
                 # Make the most recently drawn screen visible.
                 pygame.display.flip()
